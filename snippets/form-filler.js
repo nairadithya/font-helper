82d1872b9a78e3d50813ce32bd8738959e260dd1
Adithya Nair <adithyanair121@gmail.com>
2025-04-24 11:23:56 +0530

    String interpolation

--- snippets/form-filler.js
+++ snippets/form-filler.js
@@ -2,7 +2,7 @@ function formHelper() {
     var i = 1
 
     while (i <=9) {
-        a = document.getElementsByName(q${i})
+        a = document.getElementsByName(`q${i}`)
         a[0].checked = true
         i = i + 1
     }
